29ffe5e605d162eb5f11f84b83a89de5255c6411
BUPTC <48716834+BUPTC@users.noreply.github.com>
2019-09-18 12:08:26 +0800

    Create Exer1_4_CalYears.java

---
+++ Exer1_4_CalYears.java
@@ -0,0 +1,23 @@
+/*
+Editor:Chen Yihao
+Date:2019/9/15
+Content:Convert minutes to days and years(把分钟转化为年数和天数)
+*/
+package ch01.Exer1_4;
+import java.util.Scanner;
+
+public class CalYears{
+	public static void main(String args[])
+	{
+		System.out.println("请输入分钟数");
+		Scanner sc=new Scanner(System.in);
+		double Minutes=sc.nextDouble();
+		double Days=Minutes/60/24;
+		double Years=Days/365;
+		
+		sc.close();
+		
+		System.out.print("这些分钟是"+Days+"天，");
+		System.out.println(Years+"年");
+	}
+}
